f060d2cf015d992894cb50ce729c4904278c8145
Maycon Vieira <maycon.mendes@webjump.com.br>
2023-01-22 00:52:04 -0300

    #6 Object Types, Fields

--- index.js
+++ index.js
@@ -1,44 +1,70 @@
 const { gql, ApolloServer } = require( "apollo-server");
 
 /**
- * Sacalar Types
- * Int
- * Float
- * String
- * Boolean
- * ID
+ * =>Schema
+ * =>Schema Difinition Language ou linguagem de Definição de Esquema
+ * =>SDL
  */
 
+const produtos = [
+    {
+        id: 10,
+        nome: 'Intel i9',
+        valor: 5000.99
+    },
+    {
+        id: 11,
+        nome: 'Teclado ',
+        valor: 50.99
+    }
+]
+
+const usuarios = [
+    {
+        id: 1,
+        nome: 'Maycon',
+        salario: 1500.69,
+        ativo: true,
+        idade: 19
+    },
+    {
+        id: 2,
+        nome: 'Jailton',
+        salario: 3500.69,
+        ativo: true,
+        idade: 40
+    }
+]
+
 const typeDefs = gql`
-    type Query {
+
+    type Produto {
+        id: ID
+        nome: String
+        valor: Float
+    }
+
+    type Usuario {
         idade: Int
         salario: Float
         nome: String
         ativo: Boolean
         id: ID
-        tecnologias: [String!]!
+    }
+
+    type Query {
+        usuarios: [Usuario]
+        produtos: [Produto]
     }
 `;
 
 const resolvers = {
     Query: {
-        idade() {
-            return 18;
+        usuarios() {
+            return usuarios
         },
-        salario() {
-            return 3213.33
-        },
-        nome() {
-            return 'Maycon'
-        },
-        ativo() {
-            return false
-        },
-        id() {
-            return 321333
-        },
-        tecnologias() {
-            return ['GraphQL', 'ReactJS', 'CSS']
+        produtos() {
+            return produtos
         }
     }
 };
